ab7cc2730665ca56c3d88c4dfa90c4e6af88fd3a
Yvon Huynh <yvon.huynh@gmail.com>
2020-03-07 16:49:23 +0100

    factory method

--- projet3.js
+++ projet3.js
@@ -1,7 +1,17 @@
 
 //la carte interactive
+class Map{
+  constructor(){
+      
+  }
 
-var mymap = L.map('mapdiv').setView([47.2162, -1.5492], 15);
+  static factory(element,lonlat,zoomLevel){
+      return L.map(element).setView(lonlat,zoomLevel);
+  }
+}
+
+//var mymap =  L.map('mapdiv').setView([47.2162, -1.5492], 15);
+var mymap = Map.factory('mapdiv',[47.2162, -1.5492],15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic290aWJhIiwiYSI6ImNrMzR3Y3VpMjE0NmQzYnA5a3cxaTExbm4ifQ._5V7MpOr4Ivvza511fHK2w', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
